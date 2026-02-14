19c1654cb6fbf79cbed3c7687c46a40bca09f612
Jean-Romaric <romaric.akpoue@epitech.eu>
2026-02-13 21:42:19 -0700

    firt commit

---
+++ Frontend/script.js
@@ -0,0 +1,80 @@
+const notesContainer = document.getElementById("app");//gd div
+const addNoteButton = notesContainer.querySelector(".add-note");//button
+
+
+function getNotes() {
+  return JSON.parse(localStorage.getItem("sticky-notes") || "[]");
+}
+
+addNoteButton.addEventListener("click", () => addNote());
+
+function addNote() {
+  const notes = getNotes();
+  const noteObj = { id: Math.floor(Math.random() * 100000), content: "" };
+  //let a ={ id: Math.floor(Math.random() * 100000), content: "cvb" };
+  //console.log(a);
+ // console.log(noteObj);
+  //console.log(noteObj.id)//
+  //------- -----------//
+  const noteElement = createNoteElement(noteObj.id, noteObj.content);
+  console.log(noteObj.content) //chaine vide 
+
+  notesContainer.insertBefore(noteElement, addNoteButton);
+  notes.push(noteObj);
+  saveNotes(notes);
+}   
+
+function createNoteElement(id, content) {
+  const element = document.createElement("textarea");
+  element.classList.add("note");
+  content =  element.value ;//recupère la valeur du textarea(**** ****)
+  //console.log(content) //chaine vide 
+  //console.log(element.value)
+
+  element.placeholder = "Bonjour...";
+  element.addEventListener("change", () => updateNote(id, element.value));
+  //------- -----------//
+  element.addEventListener("dblclick", () => {
+    if (confirm("Supprimer cette note ?")) 
+        deleteNote(id, element);
+    });
+
+  return element;
+}
+
+
+function updateNote(id, newContent) {
+  const notes = getNotes();
+  console.log(notes)
+  const target = notes.filter(n => n.id == id);
+  target.content = newContent;
+  saveNotes(notes);
+}
+
+function deleteNote(id, element) {
+  const notes = getNotes().filter(n => n.id != id);
+  saveNotes(notes);
+  notesContainer.removeChild(element);
+}
+
+
+// Charger les notes au démarrage
+getNotes().forEach(note => {
+  const noteElement = createNoteElement(note.id, note.content);
+  notesContainer.insertBefore(noteElement, addNoteButton);
+});
+
+
+
+
+
+
+function saveNotes(notes) {
+  localStorage.setItem("sticky-notes", JSON.stringify(notes));
+}
+
+
+
+
+
+
--- models/userModel.js
+++
@@ -1,9 +0,0 @@
-//(3)
-class User{
-    constructor(nom, prenoms) {  
-        this.nom = nom;
-        this.prenoms = prenoms;
-    }
-}
-
-module.exports = { User };
